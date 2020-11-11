da9a751b62139542d248adae0c9898ac17620ced
subbus <subbareddyg83@gmail.com>
2020-11-11 23:24:53 +0530

    added deletion of employee in the database

--- src/employee.java
+++ src/employee.java
@@ -7,20 +7,15 @@ public class employee
         String url = "jdbc:mysql://localhost:3306/demo";
         String user = "root";
         String password = "1234";
-
         // get a connection to database
         Connection connection = DriverManager.getConnection(url, user, password);
-
         // create a statement
         Statement statement = connection.createStatement();
-
         // execute SQL query
-        String query = "UPDATE employees SET first_name = 'John' WHERE id = 1;";
-
-        statement.executeUpdate(query);
-
-        System.out.println("Updation complete");
-
+        String query = "DELETE FROM employees WHERE id = 2";
+        int rowsAffected = statement.executeUpdate(query);
+        System.out.println("Deletion complete");
+        System.out.println("No of rows affected:" + rowsAffected);
     }
 
 }
