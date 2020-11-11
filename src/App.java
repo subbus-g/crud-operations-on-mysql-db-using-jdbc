ef04a0234d251eb5dae85907db361526bc158b76
subbus <subbareddyg83@gmail.com>
2020-11-11 23:34:23 +0530

    refactored the code

--- src/App.java
+++
@@ -1,8 +0,0 @@
-public class App 
-{
-    public static void main(String[] args) throws Exception 
-    {
-        System.out.println("Welcome to crud-operations-on-mysql-db-using-jdbc program");
-    }
-
-}
--- src/employee.java
+++ src/employee.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-public class employee 
+public class Employee 
 {
     public static void main(String[] args) throws Exception 
     {
