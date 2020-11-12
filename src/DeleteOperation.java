c0ce2c769af39b7639c37bc03b68fd6397a241f6
subbus <subbareddyg83@gmail.com>
2020-11-12 09:20:54 +0530

    added  java file for each and every crud operation

--- src/DeleteOperation.java
+++ src/DeleteOperation.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-public class Employee 
+public class DeleteOperation 
 {
     public static void main(String[] args) throws Exception 
     {
---
+++ src/UpdateOperation.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class UpdateOperation 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        String url = "jdbc:mysql://localhost:3306/demo";
+        String user = "root";
+        String password = "1234";
+
+        // get a connection to database
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        // create a statement
+        Statement statement = connection.createStatement();
+
+        // execute SQL query
+        String query = "UPDATE employees SET first_name = 'John' WHERE id = 1;";
+
+        statement.executeUpdate(query);
+
+        System.out.println("Updation complete");
+    }
+
+}
